7330cff748faa01a105adf80176d4093f96a7e28
Expert-ShashwatTiwari <shashwattiwari6@gmail.com>
2024-11-02 00:44:16 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,63 @@
+(function ($) {
+	"use strict";
+// Header Sticky
+$(window).on('scroll',function() {
+	var stickytop = $('#header.sticky-top .bg-transparent');
+	var stickytopslide = $('#header.sticky-top-slide');
+	
+	if ($(this).scrollTop() > 1){  
+		stickytop.addClass("sticky-on-top");
+		stickytop.find(".logo img").attr('src',stickytop.find('.logo img').data('sticky-logo'));
+	}
+	else {
+		stickytop.removeClass("sticky-on-top");
+		stickytop.find(".logo img").attr('src',stickytop.find('.logo img').data('default-logo'));
+	}
+	
+	if ($(this).scrollTop() > 180){  
+		stickytopslide.find(".primary-menu").addClass("sticky-on");
+		stickytopslide.find(".logo img").attr('src',stickytopslide.find('.logo img').data('sticky-logo'));
+	}
+	else{
+		stickytopslide.find(".primary-menu").removeClass("sticky-on");
+		stickytopslide.find(".logo img").attr('src',stickytopslide.find('.logo img').data('default-logo'));
+	}
+});
+
+
+// Mobile Menu
+$('.navbar-toggler').on('click', function() {
+	$(this).toggleClass('show');
+});
+$(".navbar-nav a").on('click', function() {
+    $(".navbar-collapse, .navbar-toggler").removeClass("show");
+});
+
+/*------------------------------------
+    Typed
+-------------------------------------- */
+
+$(".typed").each(function() {
+var typed = new Typed('.typed', {
+    stringsElement: '.typed-strings',
+	loop: true,
+	typeSpeed: 100,
+    backSpeed: 50,
+	backDelay: 1500,
+});
+});
+
+
+/*------------------------
+   tooltips
+-------------------------- */
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
+
+})
+
+(jQuery)
